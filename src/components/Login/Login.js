61d511de95f6ebaa7db342eab7096d1fd6136aea
Gavin Brown <55294040+gavinbrown025@users.noreply.github.com>
2021-11-18 10:40:08 -0700

    production setup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,6 @@
 import './Login.scss'
 
-const authEndpoint = 'https://accounts.spotify.com/authorize'
-const redirectUri = 'http://localhost:3000/'
-const clientId = '8a7929a12fed4285ab9840e36fb2395c'
-const scopes = [
-    'streaming',
-    'user-top-read',
-    'user-read-email',
-    'user-read-private',
-    'user-library-read',
-    'user-library-modify',
-    'user-read-currently-playing',
-    'user-read-recently-played',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-]
-
-const AUTH_URL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=code&dialog=true`
+import { AUTH_URL } from '../../hooks/useAuth'
 
 const login = () => {
     return (
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,33 @@
-//*imrs
-import { useEffect } from 'react'
 import axios from 'axios'
+import { useEffect } from 'react'
 import { useDataLayerValue } from '../store/DataLayer'
 
-const SERVER_URL = 'https://music-server-gb.herokuapp.com'
-const DEV_URL = 'http://localhost:5000'
+const production = true
+let url = 'http://localhost:5000'
+let redirectUri = 'http://localhost:3000/'
 
-const url = SERVER_URL
+if(production){
+    url = 'https://music-server-gb.herokuapp.com'
+    redirectUri = 'https://react-player-gb.herokuapp.com/'
+}
+
+const authEndpoint = 'https://accounts.spotify.com/authorize'
+const clientId = '8a7929a12fed4285ab9840e36fb2395c'
+const scopes = [
+    'streaming',
+    'user-top-read',
+    'user-read-email',
+    'user-read-private',
+    'user-library-read',
+    'user-library-modify',
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+]
+
+export const AUTH_URL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=code&dialog=true`
+
 
 const useAuth = (code) => {
 	const [{ token }, dispatch] = useDataLayerValue()
